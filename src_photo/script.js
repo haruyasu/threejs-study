74938c32729a46da9d032662bb9b8bda4568161f
haruyasu <harukun2002@gmail.com>
2021-05-04 21:40:30 +0900

    star

--- src_photo/script.js
+++ src_photo/script.js
@@ -1,10 +1,10 @@
 import './style.css'
 import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+// import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'dat.gui'
+import gsap from 'gsap'
 
 const loader = new THREE.TextureLoader()
-const cross = loader.load('/particles/star_01.png')
 
 // Debug
 const gui = new dat.GUI()
@@ -15,37 +15,26 @@ const canvas = document.querySelector('canvas.webgl')
 // Scene
 const scene = new THREE.Scene()
 
+const geometry = new THREE.PlaneBufferGeometry(1, 1)
+
 // Objects
-const geometry = new THREE.TorusGeometry(0.7, 0.2, 16, 100)
+for (let i = 0; i < 4; i++) {
+  const material = new THREE.MeshBasicMaterial({
+    map: loader.load(`/photo/${i}.png`),
+  })
 
-const particlesGeometry = new THREE.BufferGeometry()
-const particlesCnt = 5000
+  const img = new THREE.Mesh(geometry, material)
+  img.position.set(1, i * -1.5)
 
-const posArray = new Float32Array(particlesCnt * 3)
-
-for (let i = 0; i < particlesCnt * 3; i++) {
-  posArray[i] = (Math.random() - 0.5) * (Math.random() * 5)
+  scene.add(img)
 }
 
-particlesGeometry.setAttribute('position', new THREE.BufferAttribute(posArray, 3))
-
-// Materials
-
-const material = new THREE.PointsMaterial({
-  size: 0.005
+let objs = []
+scene.traverse((object) => {
+  if (object.isMesh) {
+    objs.push(object)
+  }
 })
-
-const particlesMaterial = new THREE.PointsMaterial({
-  size: 0.02,
-  map: cross,
-  transparent: true,
-  color: 'yellow',
-})
-
-// Mesh
-const sphere = new THREE.Points(geometry, material)
-const particlesMesh = new THREE.Points(particlesGeometry, particlesMaterial)
-scene.add(sphere, particlesMesh)
 
 // Lights
 
@@ -92,6 +81,8 @@ camera.position.y = 0
 camera.position.z = 2
 scene.add(camera)
 
+gui.add(camera.position, 'y').min(-5).max(1)
+
 // Controls
 // const controls = new OrbitControls(camera, canvas)
 // controls.enableDamping = true
@@ -104,22 +95,28 @@ const renderer = new THREE.WebGLRenderer({
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-renderer.setClearColor(new THREE.Color('#21282a'), 1)
 
+// Mouse
+window.addEventListener('wheel', onMouseWheel)
+let y = 0
+let position = 0
 
-document.addEventListener('mousemove', animateParticles)
-
-let mouseX = 0
-let mouseY = 0
-
-function animateParticles(event) {
-  mouseY = event.clientY
-  mouseX = event.clientX
+function onMouseWheel(event) {
+  y = event.deltaY * 0.0007
 }
+
+const mouse = new THREE.Vector2()
+
+window.addEventListener('mousemove', (event) => {
+  mouse.x = (event.clientX / sizes.width) * 2 - 1
+  mouse.y = -(event.clientY / sizes.height) * 2 + 1
+})
 
 /**
  * Animate
  */
+
+const raycaster = new THREE.Raycaster()
 
 const clock = new THREE.Clock()
 
@@ -127,13 +124,42 @@ const tick = () => {
   const elapsedTime = clock.getElapsedTime()
 
   // Update objects
-  sphere.rotation.y = 0.5 * elapsedTime
-  particlesMesh.rotation.y = -0.1 * elapsedTime
+  position += y
+  y *= 0.9
 
-  if (mouseX > 0) {
-    particlesMesh.rotation.x = -mouseY * (elapsedTime * 0.00008)
-    particlesMesh.rotation.y = mouseX * (elapsedTime * 0.00008)
+  camera.position.y = -position
+
+  raycaster.setFromCamera(mouse, camera)
+  const intersects = raycaster.intersectObjects(objs)
+
+  for (const intersect of intersects) {
+    gsap.to(intersect.object.scale, {
+      x: 1.7,
+      y: 1.7
+    })
+    gsap.to(intersect.object.rotation, {
+      y: -0.1
+    })
+    gsap.to(intersect.object.position, {
+      z: -0.2
+    })
   }
+
+  for (const object of objs) {
+    if (!intersects.find((intersect) => intersect.object === object)) {
+      gsap.to(object.scale, {
+        x: 1,
+        y: 1,
+      })
+      gsap.to(object.rotation, {
+        y: 0,
+      })
+      gsap.to(object.position, {
+        z: 0,
+      })
+    }
+  }
+
 
   // Update Orbital Controls
   // controls.update()
